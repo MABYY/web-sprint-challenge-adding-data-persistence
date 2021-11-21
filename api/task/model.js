6972f0d15928819edfcd12c0c00098128a70a6fc
Maria Negri <mariamnegri@gmail.com>
2021-11-21 13:35:20 -0300

    wip

--- api/task/model.js
+++ api/task/model.js
@@ -1,16 +1,33 @@
 // build your `Task` model here
 const db = require('../../data/dbConfig')
 
-function findAll() {
-    return db('tasks as t')
-    .leftJoin("projects as p", "t.project_id", "=","p.project_id")
-    .select('t.task_id',
-            "t.task_description" ,
-            't.task_notes',
-            't.task_completed',
-            'p.project_name',
-            'p.project_description'
-            )
+async function findAll() {
+    const result =  await db('tasks as t')
+                        .leftJoin("projects as p", "t.project_id", "=","p.project_id")
+                        .select('t.task_id',
+                                "t.task_description" ,
+                                't.task_notes',
+                                't.task_completed',
+                                'p.project_name',
+                                'p.project_description'
+                                )
+    const result_array = !result[0].task_id ? [] :
+                    result.map(row =>{
+
+                    return {
+                        "task_id": row.task_id,
+                        "task_description": row.task_description,
+                        "task_notes":row.task_notes,
+                        "task_completed":row.task_notes == 0? false : true,
+                        "project_name":row.project_name,
+                        "project_description":row.project_description
+                
+                    }
+            })
+
+   return result_array
+  
+
 }
 
 const getById = (id) => {
