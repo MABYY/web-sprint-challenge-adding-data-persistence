16b233149261c24e7d39eea82d61247b065e9542
Maria Negri <mariamnegri@gmail.com>
2021-11-20 22:41:59 -0300

    server running

--- api/server.js
+++ api/server.js
@@ -1,1 +1,18 @@
-// build your server here and require it from index.js
+const express = require("express")
+const server = express()
+// const RecipieRoute = require('./recipie-router')
+
+server.use(express.json())
+// server.use('/api/recipie',RecipieRoute)
+
+server.get('/',(req,res) => {
+    res.send(`<h1>Sprint Challenge</h1>`)
+})
+
+server.get('*',(req,res) =>{
+    res.status(500).json({
+        message: "Incorrect Path"
+    })
+})
+
+module.exports = server     
